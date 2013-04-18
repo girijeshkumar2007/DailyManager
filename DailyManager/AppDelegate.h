3d68b7e4708c1fcdedea9d1e4ddae2ec8590e40c
Girijesh Kumar <girijeshkumar@Girijeshs-MacBook-Pro.local>
2013-04-18 10:41:20 +0530

    Initial Commit

---
+++ DailyManager/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  DailyManager
+//
+//  Created by Girijesh Kumar on 18/04/13.
+//  Copyright (c) 2013 Girijesh Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ DailyManager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DailyManager
+//
+//  Created by Girijesh Kumar on 18/04/13.
+//  Copyright (c) 2013 Girijesh Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
